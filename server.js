b7bd9437a2e617ce4dfcf96aafe10a001d5e48de
daksh <bhatidakshraj2604@hmail.com>
2026-04-30 09:26:37 +0530

    new change

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 require("dotenv").config();
 
 const app = express();
@@ -15,4 +15,23 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+*/
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
